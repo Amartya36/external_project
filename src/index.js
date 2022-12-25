ccc5b59dd7a8056f3732d71db44b690dc5ae5b38
chanda <jhachanda43@gmail.com>
2022-12-25 23:15:52 +0530

    multer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./router/route')
+const multer  = require('multer')
 const port = 3000
 
 const app = express()
+app.use(multer().any())
 app.use(express.json())
 
 
@@ -16,7 +18,7 @@ mongoose.connect('mongodb+srv://chanda:QYho3EZNKLny4znA@cluster0.gkrjc46.mongodb
 .catch((error)=>{
     console.log(error, "mongoose is not connected")
 })
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`port is connected ${port}`)
 })
     app.use('/', router)
